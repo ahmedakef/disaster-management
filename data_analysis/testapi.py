8ccbddeb6d4db8eda0ca067344f3033152a2c31a
nadamakram <31819138+nadamakram@users.noreply.github.com>
2018-09-26 01:02:46 +0200

    Add files via upload

---
+++ data_analysis/testapi.py
@@ -0,0 +1,24 @@
+import requests as req
+from pprint import pprint
+import json
+
+
+api_address = 'https://samples.openweathermap.org/data/2.5/forecast?appid=5aba5640f410444530f8355107819e82&lat={}&lon={}'
+lat = input("latitude of the city: ")
+lon = input("longtuide of the city: ")
+
+url = api_address.format( lat, lon)
+
+json_data = req.get(url).json()
+
+
+for each in json_data["list"]:
+    print(each["main"]["temp_min"][1])
+    print(each["main"]["temp_max"])
+    print(each["main"]["sea_level"])
+    print(each["wind"]["speed"])
+    print(each["rain"])
+    if each.get("rain") == {}:
+       rain = 0
+
+    
