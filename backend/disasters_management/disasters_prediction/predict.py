d91afcc9303ad1d9f857daab92c61fbeadd5908a
nadamakram <31819138+nadamakram@users.noreply.github.com>
2018-09-29 02:56:38 +0200

    Add files via upload

--- backend/disasters_management/disasters_prediction/predict.py
+++ backend/disasters_management/disasters_prediction/predict.py
@@ -1,5 +1,6 @@
 import urllib3, requests, json
-import APIweatherFinal
+#import APIweatherFinal
+from API4 import get_weather
 from backend_handler import BackendHandler  
 
 # retrieve your wml_service_credentials_username, wml_service_credentials_password, and wml_service_credentials_url from the
@@ -18,32 +19,50 @@ mltoken = json.loads(response.text).get('token')
 
 header = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + mltoken}
 
-# NOTE: manually define and pass the array(s) of values to be scored in the next line
+latWI = 43.002316
+lonWI = -89.424095
+
+latIL = 40.666149
+lonIL = -89.580101
+
+latIA = 41.619549
+lonIA = -93.598022
+
+latMN = 45.560230
+lonMN = -94.172852
+
+tuple1 = ([latWI, lonWI] , [latMN, lonMN] , [latIL, lonIL] , [latIA, lonIA])
 
 
-array_of_values_to_be_scored = [APIweatherFinal.state, None , APIweatherFinal.min_temp, APIweatherFinal.max_temp, APIweatherFinal.sea_level, None , None,  None, APIweatherFinal.wind_speed, None ]
-another_array_of_values_to_be_scored = [APIweatherFinal.state, None , APIweatherFinal.min_temp, APIweatherFinal.max_temp, APIweatherFinal.sea_level, None , None,  None, APIweatherFinal.wind_speed, None ]
-payload_scoring = {"fields": ["State", "Start Date", "min temperature(F)", "max temperature(F)", "Mean sea level pressure(IN)", "mean dew point(F)", "Total precipitation(IN)", "visibility(MI)", "Mean wind speed(MPH)", "max sustained wind speed(MPH)"], "values": [array_of_values_to_be_scored, another_array_of_values_to_be_scored]}
+def DisasterPrediction():
+  # NOTE: manually define and pass the array(s) of values to be scored in the next line
 
-response_scoring = requests.post('https://us-south.ml.cloud.ibm.com/v3/wml_instances/e57f2229-3fda-4e56-a902-f746bcbe466a/deployments/72bd12f3-ee64-4d33-9034-28f3f611aab6/online', json=payload_scoring, headers=header)
-print("Scoring response")
-#print(json.loads(response_scoring.text))
+  for element in tuple1:
+    weather = get_weather(element[0], element[1])
 
-parsed = json.loads(response_scoring.text)
-for value in parsed['values']:
-  prediction = value[-3]
-  print("The prediction is: ", prediction)
+    array_of_values_to_be_scored = [None, None , weather["min_temp"], weather["max_temp"], weather["sea_level"], None , None,  None, weather["wind_speed"], None ]
+    another_array_of_values_to_be_scored = [None, None , weather["min_temp"], weather["max_temp"], weather["sea_level"], None , None,  None, weather["wind_speed"], None ]
+    payload_scoring = {"fields": ["State", "Start Date", "min temperature(F)", "max temperature(F)", "Mean sea level pressure(IN)", "mean dew point(F)", "Total precipitation(IN)", "visibility(MI)", "Mean wind speed(MPH)", "max sustained wind speed(MPH)"], "values": [array_of_values_to_be_scored, another_array_of_values_to_be_scored]}
 
-if prediction > 0.5:
-  disaster = {
-        "diameter": 1000,
-        "level_of_danger": "D",
-        "lat"	:	APIweatherFinal.lat,
-        "lang"	:	APIweatherFinal.lon
-    }
-  response = BackendHandler.create_disaster(**disaster)
-  print(response)
+    response_scoring = requests.post('https://us-south.ml.cloud.ibm.com/v3/wml_instances/e57f2229-3fda-4e56-a902-f746bcbe466a/deployments/72bd12f3-ee64-4d33-9034-28f3f611aab6/online', json=payload_scoring, headers=header)
+    print("Scoring response")
+    #print(json.loads(response_scoring.text))
+
+    parsed = json.loads(response_scoring.text)
+    for value in parsed['values']:
+      prediction = value[-3]
+      print("The prediction is: ", prediction)
+
+    if prediction > 0.5:
+      disaster = {
+            "diameter": 1000,
+            "level_of_danger": "D",
+            "lat"	:	element[0],
+            "lang"	:	element[1]
+        }
+      response = BackendHandler.create_disaster(**disaster)
+      print(response)
 
 
 
- 
+DisasterPrediction()
